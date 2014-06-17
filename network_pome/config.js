b543980ed78117969364e73ae50fdf3a036bebff
ghirardi nicola <ghirardi.nicola@gmail.com>
2014-06-17 11:58:57 +0200

    fixed networ pome data

--- network_pome/config.js
+++ network_pome/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 setParams({
-    graphFile : "../data/mentions_network_afternoon.gexf",
+    graphFile : "mentions_network_afternoon.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
